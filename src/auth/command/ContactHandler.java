e6d55e0f04d70fed3b14e5b6143f1421689001bb
kimth007kim <kimth007kim@naver.com>
2020-02-06 14:19:41 +0900

    지도 추가중

--- src/auth/command/ContactHandler.java
+++ src/auth/command/ContactHandler.java
@@ -1,23 +1,23 @@
 package auth.command;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import auth.service.LoginFailException;
 import auth.service.LoginService;
-import auth.service.User;
 import mvc.command.CommandHandler;
 
 public class ContactHandler implements CommandHandler {
 
 	private static final String FORM_VIEW = "/template/contact.jsp";
+	private LoginService loginService = new LoginService();
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) 
 	throws Exception {
+		return processForm(req,res);
+	}
+	
+	private String processForm(HttpServletRequest req, HttpServletResponse res) {
 		return FORM_VIEW;
 	}
 
